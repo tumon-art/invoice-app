06cc5333ceb17d8bdcfea18e7c322d2b55718f58
tumon <mofakhar0@gmail.com>
2024-10-16 20:15:28 +0600

    .

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -8,23 +9,29 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { CirclePlus } from 'lucide-react';
 
 export default function Home() {
   return (
     <main className=" h-screen flex flex-col 
       justify-center gap-5 text-center md:mx-10">
 
-      <h1 className="text-5xl font-bold"> Invoic App</h1>
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold"> Invoic App</h1>
+        <Button variant="ghost" className="inline-flex gap-2">
+          <CirclePlus className="h-4 w-4" />
+          Create Invoice </Button>
+      </div>
 
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">Date</TableHead>
-            <TableHead>Customer</TableHead>
-            <TableHead>Email</TableHead>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
             <TableHead className="text-center">Status</TableHead>
-            <TableHead>Value</TableHead>
+            <TableHead className="p-4">Value</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -39,6 +46,6 @@ export default function Home() {
           </TableRow>
         </TableBody>
       </Table>
-    </main>
+    </main >
   );
 }
