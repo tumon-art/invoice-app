393b9002a4ea64a17db2a496ebf75ca49adac3e9
tumon <mofakhar0@gmail.com>
2024-10-18 03:54:58 +0600

    .

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { integer, pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const statusEnum = pgEnum('status', ['open', 'paid', 'void', 'uncollectible'])
+export const Invoices = pgTable('invoices', {
+  id: serial('id').primaryKey().notNull(),
+  created_at: timestamp('created_at').notNull().defaultNow(),
+  value: integer('value').notNull(),
+  description: text('description').notNull(),
+  status: statusEnum('status').notNull()
+})
