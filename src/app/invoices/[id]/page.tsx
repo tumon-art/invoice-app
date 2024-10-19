cc92258cb12d92c7f96aacbb8aec179d736ccdc5
tumon <mofakhar0@gmail.com>
2024-10-19 18:47:17 +0600

    .

--- src/app/invoices/[id]/page.tsx
+++ src/app/invoices/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { db } from "@/db";
 import { InvoicesSchema } from "@/db/schema";
 import { eq } from "drizzle-orm";
@@ -11,13 +12,41 @@ export default async function InvoicePage({ params }: { params: { id: string } }
     .limit(1)
 
   return (
-    <main className=" h-screen flex flex-col 
-      justify-center gap-5 text-center md:mx-10">
+    <main className="flex flex-col 
+      mt-20 gap-5 text-center md:mx-10">
 
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-bold"> Invoic {invoiceId} </h1>
+      <div className="flex gap-6 items-center">
+        <h1 className="text-3xl font-bold"> Invoice {invoiceId} </h1>
+        <span>
+          <Badge className="rounded-full">
+            {getInvoice.status}
+          </Badge>
+        </span>
       </div>
 
+      {/* TOTAL AMMOUNT  */}
+      <h1 className=" mt-6 text-left text-3xl">
+        $ {getInvoice.value.toFixed(2)}
+      </h1>
+
+      {/* Description  */}
+      <p className="text-left" > {getInvoice.description} </p>
+
+      <h3 className="mt-4 text-left text-lg font-semibold">
+        Billing Details
+      </h3>
+
+      <div className=" text-left">
+        <div className=" flex gap-10">
+          <p> Invoice ID </p> <span> {getInvoice.id} </span>
+        </div>
+
+
+        <div className=" flex gap-10">
+          <p> Invoice Date </p> <span> {new Date(getInvoice.created_at).toLocaleDateString()} </span>
+        </div>
+
+      </div>
     </main >
   );
 }
