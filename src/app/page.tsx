9e6a5c5df818a7adf11e436b73643882fe2c3ed2
tumon <mofakhar0@gmail.com>
2024-10-23 00:01:32 +0600

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className=" h-screen flex flex-col 
-      justify-center gap-5 text-center mx-auto">
+      justify-center text-center mx-auto">
 
-      <h1 className="text-5xl font-bold"> Invoic Signup </h1>
-      <p>
-        <Button asChild>
-          <Link href="/dashboard"> Sign in </Link>
-        </Button>
-      </p>
+      <div className=" flex flex-col gap-20">
+        <h1 className="text-5xl font-bold"> Invoic App </h1>
+        <p>
+          <Button asChild>
+            <Link href="/dashboard"> Open Dashbard </Link>
+          </Button>
+        </p>
+      </div>
     </main>
   );
 }
