b3a41c2569a41d5b078c2fba18420a798bb1b3bf
tumon <mofakhar0@gmail.com>
2024-10-27 05:35:49 +0600

    .

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,10 +4,11 @@ import { Customers, InvoicesSchema, Status } from "@/db/schema"
 import { auth } from "@clerk/nextjs/server"
 import { and, eq } from "drizzle-orm"
 import { revalidatePath } from "next/cache"
+import { headers } from "next/headers"
 import { redirect } from "next/navigation"
-// import { Stripe } from "stripe"
+import { Stripe } from "stripe"
 
-// const stripe = new Stripe(String(process.env.STRIPE_API_KEY))
+const stripe = new Stripe(String(process.env.STRIPE_API_KEY))
 
 export async function addInvoice(formData: FormData) {
   const { userId } = auth();
@@ -65,6 +66,8 @@ export async function deleteInvoiceAction(formData: FormData) {
 }
 
 export const createPayment = async (formData: FormData) => {
+  const headerList = headers()
+  const origin = headerList.get("origin")
   const id = Number(formData.get("id"))
 
   const [results] = await db.select({
@@ -74,5 +77,26 @@ export const createPayment = async (formData: FormData) => {
     .from(InvoicesSchema)
     .where(eq(InvoicesSchema.id, id))
     .limit(1)
+
+  // Create Checkout Sessions from body params.
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product: "prod_R6WLf6UZWgknSm",
+          unit_amount: results.value,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${origin}/invoices/${id}/payment?success=true`,
+    cancel_url: `${origin}/invoices/${id}/payment?canceled=true`,
+  });
+
+  if (!session.url) throw new Error("invalid  session")
+
+  redirect(session.url)
 }
 
